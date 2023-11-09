4dfad43942def65a90fcd94964ad488a07d8a642
Oliver <18360865+ost-ing@users.noreply.github.com>
2023-11-09 17:35:07 +0100

    add support for multiple models in the family

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
-# is31fl3205
-A rust-embedded driver for the Lumissil Microsystems IS31FL3205 LED driver
+# is31fl32xx
+A rust-embedded driver for the Lumissil Microsystems IS31FL32xx LED driver
 
-- https://crates.io/crates/is31fl3205
+- https://crates.io/crates/is31fl32xx
 
-## description
-The IS31FL3205 is an LED driver with 12 constant current channels. Each channel can be pulse width modulated (PWM) by 16 bits for smooth LED brightness control. In addition, each channel has an 8-bit output current control register which allows fine tuning the current for rich RGB color mixing, e.g., a pure white color LED application. The maximum output current of each channel can be adjusted by one 8-bit global control register.
+## About 
+The IS31FL3205 and IS31FL3237 are LED drivers with 12 and 36 constant current channels respectively. Each channel can be pulse width modulated (PWM) by 16 bits for smooth LED brightness control. In addition, each channel has an 8-bit output current control register which allows fine tuning the current for rich RGB color mixing, e.g., a pure white color LED application. The maximum output current of each channel can be adjusted by one 8-bit global control register.
 
-Proprietary programmable algorithms are used in IS31FL3205 to minimize audible noise caused by the MLCC decoupling capacitor. All registers can be programmed via a high speed I2C (1MHz).
-IS31FL3205 can be turned off with minimum current consumption by either pulling the SDB pin low or by using the software shutdown feature.
-IS31FL3205 is available in QFN-20 (3mm × 3mm) package. It operates from 2.7V to 5.5V over the temperature range of -40°C to +125°C.
+This crate provides both blocking and DMA compatible APIs (via static callbacks) for applications utilising `embedded_hal` traits.
 
-## example
+## Example
 Examples are based on the `stm32h7xx_hal`.
 
 ```rust
@@ -32,13 +30,13 @@ let en = en.into_push_pull_output();
 
 // Initialize 
 let i2c: stm32h7xx_hal::i2c::I2c<I2C1> =
-    i2c.i2c((scl, sda), 1.MHz(), prec, clocks);
+    i2c.i2c((scl, sda), 1_u32.MHz(), prec, clocks);
 
-// Initialize with blocking I2C for device at address 0x00
-let mut interface = Is31fl3205::init_with_i2c(0x00, i2c, en);
+// Initialize with blocking I2C for device at address 0x00.
+// The either the IS31FL3205 or IS31FL3237 models are supported
+let mut interface = Is31fl32xx::<IS31FL3237, _, _>::init_with_i2c(0x00, en, i2c);
 
-// Enable the device, bringing the enable line high and configuring
-// the oscillator clock, pwm resolution
+// Enable the device, bringing the enable line high and configuring the oscillator clock, pwm resolution
 interface.enable_device(
     delay,
     OscillatorClock::SixteenMHz,
@@ -53,23 +51,22 @@ interface.set_global_current(0xFF).unwrap();
 // Set brightness scaling for all leds
 interface.set_all_led_scaling(0xFF).unwrap();
 
-/// Finally set the first LED to maximum 16bit brightness. For drivers configured for less than 16bit
-/// Only the required bits are used
+/// Finally set the first LED to maximum 16bit brightness. For drivers configured for less than 16bit Only the required bits are used
 interface.set(0, 0xFFFF).unwrap()
 
 // Release the blocking i2c example to regain access to its underlying resources
-let (_i2c, _en) = color_controller.release();
+let (_i2c, _en) = interface.release();
 
 // Additionally, if you need to integrate this driver with platform specific DMA controllers then
 // a flexible callback can be used rather than blocking i2c
 static mut DMA_BUFFER: [u8; 3] = [0; 3];
-let interface = Is31fl3205::init_with_callback(0x00, en, |addr, data| unsafe {
-    // Copy the data from the Is31fl3205 into the DMA buffer for processing
+let interface = Is31fl32xx::<IS31FL3237, _, _>::init_with_callback(0x00, en, |addr, data| unsafe {
+    // Copy the data from the Is31fl32xx into the DMA buffer for processing
     // NOTE: data can change in size depending on the message, the DMA implementation will
     // need to be able to handle changing slice lengths
     DMA_BUFFER[0..data.len()].copy_from_slice(data);
 });
 ```
 
-## contributing
+## Contributing
 Feel free to create a ticket and a MR for any changes you would like to see in this library.
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
-//! # IS31FL3205 library
-//! A rust-embedded driver for the Lumissil Microsystems IS31FL3205 LED driver
+//! # IS31FL32xx library
+//! A rust-embedded driver for the Lumissil Microsystems IS31FL32xx LED driver
+//! Currently supports the IS31FL3205 and IS31FL3237
 
 #![no_std]
-#![deny(warnings)]
+#![allow(unused)]
 
+use core::marker::PhantomData;
 use embedded_hal::blocking::delay::DelayMs;
 use embedded_hal::{
     blocking::i2c::{Read, Write, WriteRead},
@@ -24,37 +26,90 @@ pub enum Error<I> {
     InterfaceConfig,
     /// Enable line
     EnableLine,
+    /// Channel is beyond the supported number
+    ChannelOutOfBounds,
 }
 
+/// Model
+pub trait Model {
+    /// Retrieve the register value for the specific model
+    fn register_value(register: Register) -> u8;
+    /// Retreive the channel count for the specific count
+    fn channel_count() -> usize;
+}
+
+/// The IS31FL3205 is an LED driver with 12 constant current channels.
+pub struct IS31FL3205;
+impl Model for IS31FL3205 {
+    fn register_value(register: Register) -> u8 {
+        match register {
+            Register::PowerControl => 0x00,
+            Register::Pwm => 0x07,
+            Register::Update => 0x49,
+            Register::LedScaling => 0x4D,
+            Register::GlobalCurrentControl => 0x6E,
+            Register::PhaseDelayClockPhase => 0x70,
+            Register::OpenShortDetectEnable => 0x71,
+            Register::LedOpenShort => 0x72,
+            Register::TemperatureSensor => 0x77,
+            Register::SpreadSpectrum => 0x78,
+            Register::Reset => 0x7F,
+        }
+    }
+
+    fn channel_count() -> usize {
+        12
+    }
+}
+
+/// The IS31FL3237 is an LED driver with 36 constant current channels.
+pub struct IS31FL3237;
+impl Model for IS31FL3237 {
+    fn register_value(register: Register) -> u8 {
+        match register {
+            Register::PowerControl => 0x00,
+            Register::Pwm => 0x01,
+            Register::Update => 0x49,
+            Register::LedScaling => 0x4A,
+            Register::GlobalCurrentControl => 0x6E,
+            Register::PhaseDelayClockPhase => 0x70,
+            Register::OpenShortDetectEnable => 0x71,
+            Register::LedOpenShort => 0x72,
+            Register::TemperatureSensor => 0x77,
+            Register::SpreadSpectrum => 0x78,
+            Register::Reset => 0x7F,
+        }
+    }
+
+    fn channel_count() -> usize {
+        36
+    }
+}
+
+#[derive(Copy, Clone)]
 pub enum Register {
     /// Power control register
-    /// The Control Register sets software shutdown mode, internal oscillator clock frequency and PWM resolution.
-    /// The internal oscillator clock frequency and the PWM resolution will decide the output PWM frequency.
-    /// Recommend selecting PWM frequency higher than 20kHz to avoid MLCC audible noise as shown in Table 4.
-    PowerControl = 0x00,
-    /// Channel [12:1] PWM register byte
-    /// Each output has 2 bytes to modulate the PWM duty in 256/1024/4096/65536 steps. If using 8 bit PWM resolution, only PWM_L bits need to be set.
-    /// The value of the SL (Scaling Register) decides the peak current of each LED noted as IOUT.
-    /// IOUT and the value of the PWM Registers decide the average current of each LED noted as ILED.
-    Pwm = 0x07,
+    PowerControl,
+    /// PWM register
+    Pwm,
     /// Update the PWM and Scaling data
-    Update = 0x49,
+    Update,
     /// Update the PWM and Scaling data
-    LedScaling = 0x4D,
+    LedScaling,
     /// Control Global DC current/SSD
-    GlobalCurrentControl = 0x6E,
+    GlobalCurrentControl,
     /// Phase Delay and Clock Phase
-    PhaseDelayClockPhase = 0x70,
+    PhaseDelayClockPhase,
     /// Open short detect enable
-    OpenShortDetectEnable = 0x71,
+    OpenShortDetectEnable,
     /// Open short information. TODO
-    LedOpenShort = 0x72,
+    LedOpenShort,
     /// Temperature information. TODO
-    TemperatureSensor = 0x77,
+    TemperatureSensor,
     /// Spread spectrum control register. TODO
-    SpreadSpectrum = 0x78,
+    SpreadSpectrum,
     /// Reset all registers
-    Reset = 0x7F,
+    Reset,
 }
 
 pub enum SoftwareShutdownMode {
@@ -86,7 +141,7 @@ pub enum OpenShortDetect {
     OpenDetectEnable = 0x03,
 }
 
-pub struct Is31fl3205<I2C, EN> {
+pub struct Is31fl32xx<MODEL, I2C, EN> {
     /// `embedded-hal` compatible I2C instance
     interface: Option<I2C>,
     /// Transfer callback
@@ -95,18 +150,23 @@ pub struct Is31fl3205<I2C, EN> {
     address: u8,
     // Enable line
     enable: EN,
+    /// Model
+    model: PhantomData<MODEL>,
 }
-/// Each of the IS31FL3205 messages are defined by the Message struct.
-/// These provide an interface to the transferred data to modify or read registers of the IS31FL3205
-pub struct Message {
+
+/// Each of the IS31FL32xx messages are defined by the Message struct.
+/// These provide an interface to the transferred data to modify or read registers of the IS31FL32xx
+pub struct Message<MODEL> {
     /// The register to write or read to/from
-    register: Register,
+    pub register: Register,
     /// An offset of the base register address
-    register_offset: u8,
+    pub register_offset: u8,
     /// The data buffer, with a statically allocated 24 bytes
-    data: [u8; 24],
+    pub data: [u8; 24],
     /// The length of the transferred data
-    data_length: usize,
+    pub data_length: usize,
+    /// Model
+    model: PhantomData<MODEL>,
 }
 
 /// TODO: Add support for advanced features:
@@ -114,9 +174,11 @@ pub struct Message {
 /// - spread spectrum for EMI reduction techniques
 /// - open / short functionality and detection
 /// - temperature detection
-impl Message {
-    /// Creates a new Message.
-    /// This function should not be called directly
+impl<MODEL> Message<MODEL>
+where
+    MODEL: Model,
+{
+    /// Creates a new Message
     fn new(register: Register, raw_data: &[u8]) -> Self {
         let mut data = [0u8; 24];
         data[0..raw_data.len()].copy_from_slice(raw_data);
@@ -125,8 +187,14 @@ impl Message {
             data,
             data_length: raw_data.len(),
             register_offset: 0,
+            model: PhantomData,
         }
     }
+
+    pub fn register_value(&self) -> u8 {
+        MODEL::register_value(self.register)
+    }
+
     /// Defines a register offset, for example when targeting a specific channel
     fn register_offset(mut self, offset: u8) -> Self {
         self.register_offset = offset;
@@ -169,12 +237,13 @@ impl Message {
     }
 }
 
-impl<I2C, EN, S> Is31fl3205<I2C, EN>
+impl<MODEL, I2C, EN, S> Is31fl32xx<MODEL, I2C, EN>
 where
+    MODEL: Model,
     I2C: Write<u8, Error = S> + Read<u8, Error = S> + WriteRead<u8, Error = S>,
     EN: OutputPin,
 {
-    /// Initialize the Is31fl3205 with a flexible asynchronous callback interface
+    /// Initialize the Is31fl32xx with a flexible asynchronous callback interface
     /// * `address` - User definable address which should associate with the physical AD pin(s) of the device
     /// * `en` - The enable line
     /// * `callback` - Callback for custom transmission of the address and dataframe.
@@ -184,38 +253,40 @@ where
             interface: None,
             transfer_callback: Some(callback),
             enable: en,
+            model: PhantomData,
         }
     }
 
-    /// Initialize the Is31fl3205 with blocking i2c
+    /// Initialize the ≈ with blocking i2c
     /// * `address` - User definable address which should associate with the physical AD pin(s) of the device
     /// * `en` - The enable line
-    /// * `callback` - Callback for custom transmission of the address and dataframe.
+    /// * `i2c` - i2c interface
     pub fn init_with_i2c(address: u8, en: EN, i2c: I2C) -> Self {
         Self {
             address,
             interface: Some(i2c),
             transfer_callback: None,
             enable: en,
+            model: PhantomData,
         }
     }
 
     /// Release underlying resources
     pub fn release(self) -> (EN, Option<I2C>) {
-        return (self.enable, self.interface);
+        (self.enable, self.interface)
     }
 
-    /// Write a Message to the Is31fl3205, will either use the blocking i2c interface
+    /// Write a Message to the Is31fl32xx, will either use the blocking i2c interface
     /// Or write to the callback for DMA / Interrupt asynchronous communication
-    fn write(&mut self, message: Message) -> Result<(), Error<S>> {
-        let mut buff = [0u8; 24];
+    fn write(&mut self, message: Message<MODEL>) -> Result<(), Error<S>> {
+        let mut buff: [u8; 24] = [0u8; 24];
 
         // Take the first 2 bits of the user configurable address and
         // OR it with the hardcoded manufacturer address
         let address = 0x34 | ((self.address & 0x3) << 1);
 
         let data = &mut buff[0..message.data_length + 1];
-        data[0] = message.register as u8 + message.register_offset;
+        data[0] = message.register_value() + message.register_offset;
         data[1..message.data_length + 1].copy_from_slice(&message.data[0..message.data_length]);
 
         if self.interface.is_some() {
@@ -245,9 +316,10 @@ where
     ) -> Result<(), Error<S>> {
         // Set the enable line low to shutdown the device
         self.enable.set_low().map_err(|_| Error::EnableLine)?;
-        delay.delay_ms(10 as u8);
+        delay.delay_ms(10_u8);
         // Enable line must be pulled high for operation
         self.enable.set_high().map_err(|_| Error::EnableLine)?;
+        delay.delay_ms(10_u8);
         self.write(Message::power_control(osc, pms, ssd))
     }
 
@@ -259,8 +331,8 @@ where
     /// Set all leds to have the desired led scaling, a value of 0xFF will use the maximum current with respect to the global current
     /// configuration. See user manual for more information
     pub fn set_all_led_scaling(&mut self, value: u8) -> Result<(), Error<S>> {
-        for i in 0..12 {
-            self.write(Message::led_scaling(i, value))?;
+        for i in 0..MODEL::channel_count() {
+            self.write(Message::led_scaling(i as u8, value))?;
         }
         Ok(())
     }
@@ -280,8 +352,14 @@ where
         self.enable.set_low().map_err(|_| Error::EnableLine)
     }
 
-    /// Set the desired channel value. When operating in modes less than 8bit, then only the desired number of bits will be considered
+    /// Set the desired channel value.
+    /// * `channel` - index of led starting at 0
+    /// * `value` - When operating in modes less than 16bit, then only the desired number of bits will be considered
     pub fn set(&mut self, channel: u8, value: u16) -> Result<(), Error<S>> {
+        if channel as usize > MODEL::channel_count() - 1 {
+            return Err(Error::ChannelOutOfBounds);
+        }
+
         self.write(Message::pulse_width_modulation(channel, value))?;
         self.write(Message::update())
     }
